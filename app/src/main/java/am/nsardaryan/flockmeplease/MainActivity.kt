2dc697f8098d29246a975e275949a071ea3187a8
nsardaryan <nsardaryan@solit-clouds.ru>
2024-01-22 17:51:52 +0300

    move settings to bottom sheet

--- app/src/main/java/am/nsardaryan/flockmeplease/MainActivity.kt
+++ app/src/main/java/am/nsardaryan/flockmeplease/MainActivity.kt
@@ -1,19 +1,40 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package am.nsardaryan.flockmeplease
 
+import am.nsardaryan.flockmeplease.ui.theme.Background
+import am.nsardaryan.flockmeplease.ui.theme.BoidColor
 import am.nsardaryan.flockmeplease.ui.theme.FlockmepleaseTheme
+import am.nsardaryan.flockmeplease.ui.theme.Typography
+import am.nsardaryan.flockmeplease.ui.theme.BottomColor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,14 +42,13 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.withFrameNanos
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +71,6 @@ fun FlockScreen() {
 
     var boidsCount by remember { mutableIntStateOf(60) }
     val flock = remember { Flock() }
-
     LaunchedEffect(boidsCount) {
         flock.generateFlock(boidsCount)
         while (true) {
@@ -61,76 +80,98 @@ fun FlockScreen() {
         }
     }
 
-    Column() {
+    BottomSheetScaffold(
+        sheetContent = {
+            FlockSettings(
+                count = boidsCount,
+                onCountChange = { boidsCount = it },
+                separation = flock.separationValue,
+                onSeparationChange = { flock.separationValue = it },
+                cohesion = flock.cohesionValue,
+                onCohesionChange = { flock.cohesionValue = it },
+                align = flock.alignValue,
+                onAlignChange = { flock.alignValue = it }
+            )
+        },
+        sheetDragHandle = null,
+        sheetContainerColor = BottomColor,
+        sheetShape = RoundedCornerShape(16.dp, 16.dp, 0.dp, 0.dp),
+        sheetPeekHeight = 72.dp
+    ) {
         Canvas(
             modifier = Modifier
-                .weight(1f)
                 .fillMaxSize()
-                .padding(bottom = 20.dp)
+                .background(color = Background)
                 .onGloballyPositioned { coordinates ->
-                    flock.canvasHeight = coordinates.size.height - 100
-                    flock.canvasWidth = coordinates.size.width - 100
+                    flock.canvasHeight = coordinates.size.height
+                    flock.canvasWidth = coordinates.size.width
                 }
         ) {
             flock.boids.forEach { b ->
-                val mainBoidColor = b.getColorForBoid(flock.currentBoid)
                 drawCircle(
-                    color = mainBoidColor,
-                    radius = 5f,
+                    color = BoidColor,
+                    radius = 2f,
                     center = b.position
                 )
-                drawCircle(
-                    brush = Brush.radialGradient(
-                        listOf(mainBoidColor, Color.Transparent),
-                        center = b.position,
-                        radius = VISIBLE_RANGE
-                    ),
-                    alpha = 0.01f,
-                    radius = VISIBLE_RANGE,
-                    center = b.position
-                )
-                drawCircle(
-                    brush = Brush.radialGradient(
-                        listOf(mainBoidColor, Color.Transparent),
-                        center = b.position,
-                        radius = PROTECTED_RANGE
-                    ),
-                    alpha = 0.01f,
-                    radius = PROTECTED_RANGE,
-                    center = b.position
-                )
-                 drawArrow(b.position, b.speed, color = mainBoidColor)
             }
         }
-
-        Column(modifier = Modifier.padding(25.dp)) {
-            Text(text = "Forces: ${flock.currentBoid?.forces}")
-            Text(text = "Speed: ${flock.currentBoid?.speed}")
-
-            Text(text = "Boids count: $boidsCount")
-            Slider(value = boidsCount.toFloat(),
-                valueRange = 0f..250f,
-                onValueChange = { boidsCount = it.toInt() })
-
-            Text(text = "Separation: ${flock.separationValue}")
-            Slider(value = flock.separationValue,
-                valueRange = 0f..1f,
-                onValueChange = { flock.separationValue = it })
-
-            Text(text = "Cohesion: ${flock.cohesionValue}")
-            Slider(value = flock.cohesionValue,
-                valueRange = 0f..1f,
-                onValueChange = { flock.cohesionValue = it })
-
-            Text(text = "Align: ${flock.alignValue}")
-            Slider(value = flock.alignValue,
-                valueRange = 0f..1f,
-                onValueChange = { flock.alignValue = it })
-        }
-
     }
 }
 
+@Composable
+fun FlockSettings(
+    count: Int,
+    onCountChange: (Int) -> Unit,
+    separation: Float,
+    onSeparationChange: (Float) -> Unit,
+    cohesion: (Float),
+    onCohesionChange: (Float) -> Unit,
+    align: Float,
+    onAlignChange: (Float) -> Unit,
+) {
+    // Sheet content
+    Column(modifier = Modifier.padding(vertical = 12.dp, horizontal = 25.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Settings", style = Typography.headlineMedium)
+            IconButton(onClick = {
+                onSeparationChange.invoke(0f)
+                onCohesionChange.invoke(0f)
+                onAlignChange.invoke(0f)
+            }) {
+                Icon(imageVector = Icons.Filled.Refresh, "Reset")
+
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = "Boids count: $count")
+        Slider(
+            value = count.toFloat(),
+            colors = SliderDefaults.colors(),
+            valueRange = 0f..250f,
+            onValueChange = { onCountChange.invoke(it.toInt()) })
+
+        Text(text = "Separation: ${(separation * 100).roundToInt()}")
+        Slider(value = separation,
+            valueRange = 0f..1f,
+            onValueChange = { onSeparationChange.invoke(it) })
+
+        Text(text = "Cohesion: ${(cohesion * 100).roundToInt()}")
+        Slider(value = cohesion,
+            valueRange = 0f..1f,
+            onValueChange = { onCohesionChange.invoke(it) })
+
+        Text(text = "Align: ${(align * 100).roundToInt()}")
+        Slider(value = align,
+            valueRange = 0f..1f,
+            onValueChange = { onAlignChange.invoke(it) })
+    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
--- app/src/main/java/am/nsardaryan/flockmeplease/ui/theme/Color.kt
+++ app/src/main/java/am/nsardaryan/flockmeplease/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xFF051923)
+val BoidColor = Color(0xFFF5F5F5)
+val BottomColor = Color(0xFFE9E9E9)
+val slider = Color(0xFF264653)
